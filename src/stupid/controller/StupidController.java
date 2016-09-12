d149f1edcd183b1215447826e6d14c28bb672848
Bmar21 <brandonmalor21@gmail.com>
2016-09-12 13:13:43 -0600

    adding more questions and Int & Double

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -23,7 +23,58 @@ public class StupidController
 	private void askQuestions()
 	{
 		System.out.println("What is your name?");
-		String userInput = readKeyboardInput.nextLine();
-		System.out.print("you said: " + userInput);
+		String userInput1 = readKeyboardInput.nextLine();
+		System.out.println("You said: " + userInput1);
+		readKeyboardInput.nextLine(); // Used to consume the remainder of the text buffer
+		
+		System.out.println("What is your favorite animal? ");
+		String userInput2 = readKeyboardInput.nextLine();
+		System.out.println("You said: " + userInput2);
+		readKeyboardInput.nextLine(); // Used to consume the remainder of the text buffer
+		
+		System.out.println("If you could go anywhere in the world where would it be? ");
+		String userInput3 = readKeyboardInput.nextLine();
+		System.out.println("You said: " + userInput3);
+		readKeyboardInput.nextLine(); // Used to consume the remainder of the text buffer
+		
+		System.out.println("What is your favorite color? ");
+		String userInput4 = readKeyboardInput.nextLine();
+		System.out.println("You said: " + userInput4);
+		readKeyboardInput.nextLine(); // Used to consume the remainder of the text buffer
+		
+		System.out.println("what is your most beloved Video Game? ");
+		String userInput5 = readKeyboardInput.nextLine();
+		System.out.println("You said: " + userInput5);
+		readKeyboardInput.nextLine(); // Used to consume the remainder of the text buffer
+		
+		System.out.println("Who is your best friend? ");
+		String userInput6 = readKeyboardInput.nextLine();
+		System.out.println("You said: " + userInput6);
+		readKeyboardInput.nextLine(); // Used to consume the remainder of the text buffer
+		
+		System.out.println("What is your dream job? ");
+		String userInput7 = readKeyboardInput.nextLine();
+		System.out.println("You said: " + userInput7);
+		readKeyboardInput.nextLine(); // Used to consume the remainder of the text buffer
+		
+		System.out.println("what is your weigh in kilograms? ");
+		double yourWeight = readKeyboardInput.nextDouble();
+		System.out.println("You said: " + yourWeight);
+		readKeyboardInput.nextLine(); // Used to consume the remainder of the text buffer
+		
+		System.out.println("How old are you? ");
+		int yourAge = readKeyboardInput.nextInt();
+		System.out.println("You said: " + yourAge);
+		readKeyboardInput.nextLine(); // Used to consume the remainder of the text buffer
+		
+		System.out.println("What is your favorite food? ");
+		String userInput10 = readKeyboardInput.nextLine();
+		System.out.println("You said: " + userInput10);
+		readKeyboardInput.nextLine(); // Used to consume the remainder of the text buffer
+		
+		
+		
+		
+		
 	}
 }
